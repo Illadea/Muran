476f5e181ede777fc40a0d0a5360a7ab1c5388e7
Polina Sinitsyna <Pasi6@yandex.ru>
2021-02-12 16:55:53 +0300

    upd

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+////////// Hide Navbar on Scroll Down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-175px";
+  }
+  prevScrollpos = currentScrollPos;
+}
