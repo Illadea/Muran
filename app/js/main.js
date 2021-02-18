c19d0611c6f467ccc800ca3ea61d43ecc9ea3023
Polina Sinitsyna <Pasi6@yandex.ru>
2021-02-18 20:26:05 +0300

    upd

--- app/js/main.js
+++ app/js/main.js
@@ -13,7 +13,18 @@ window.onscroll = function() {
 
 /* NAVIGATION, BURGER MENU */
 $(document).ready(function() {
-  $('.menu__hamburger-btn').click(function() {
-      $('.menu__hamburger-btn').toggleClass('open-menu');
+  $('.header__hamburger-btn').click(function() {
+      $('.header__hamburger-btn').toggleClass('open-menu');
+      $('.menu__list').toggleClass('open-menu');
+      $('body').toggleClass('fixed-page');
   });
 });
+
+
+// else {
+//   if( screen.width < 1100) {
+//     document.getElementById("header").style.top = "-1px";
+//   } else {
+//     document.getElementById("header").style.top = "-175px";
+//   }
+// }
